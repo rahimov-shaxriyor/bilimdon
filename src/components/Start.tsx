136599d0109c8e1514b037925eb20d7579968d06
shaxriyor <rahimovshaxriyor1909@gmail.com>
2025-11-06 22:21:45 +0500

    updated

--- src/components/Start.tsx
+++ src/components/Start.tsx
@@ -7,7 +7,7 @@ const Start = () => {
             <h1 className="font-bold text-[2.5rem]">Bilimdon</h1>
             <div className="flex flex-col items-center gap-7.5 w-full max-w-75">
                 <Link to={'/category'}>
-                    <img src="src/assets/Group 45.svg" alt="play" width={150} height={150}/>
+                    <img src="public/Group 45.svg" alt="play" width={150} height={150}/>
                 </Link>
                 <Link to={'/documentation'} className="cursor-pointer bg-blue px-16 py-2 rounded-[2.5rem] text-[1.375rem]">KO'RSATMALAR</Link>
             </div>
